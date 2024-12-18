8b22f194d4daa1bf943f2a209b7474a73897cf92
idle-lab <idlecyb@163.com>
2024-12-18 10:47:32 +0800

    finish p4/t1:timestamps

--- src/concurrency/watermark.cpp
+++ src/concurrency/watermark.cpp
@@ -10,10 +10,22 @@ auto Watermark::AddTxn(timestamp_t read_ts) -> void {
   }
 
   // TODO(fall2023): implement me!
+  if (read_ts < watermark_) {
+    watermark_ = read_ts;
+  }
+  current_reads_[read_ts]++;
 }
 
 auto Watermark::RemoveTxn(timestamp_t read_ts) -> void {
   // TODO(fall2023): implement me!
+  if (current_reads_[read_ts] <= 0) {
+    throw Exception("number of read ts < 0");
+  }
+  current_reads_[read_ts]--;
+  while (!current_reads_.empty() && current_reads_[watermark_] == 0) {
+    current_reads_.erase(watermark_);
+    watermark_++;
+  }
 }
 
 }  // namespace bustub
