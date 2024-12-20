7751f0a64e07a33afcaf09850085bb2e5f9128a6
idle-lab <idlecyb@163.com>
2024-12-20 18:22:05 +0800

    finish p4/t4 Primary Key Index

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -28,7 +28,7 @@ void InsertExecutor::Init() {
   child_executor_->Init();
 }
 
-auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
+auto InsertExecutor::Next(Tuple *tuple, RID *rid) -> bool {
   // this func only execute once
   if (is_over_) {
     return false;
@@ -37,21 +37,45 @@ auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
   Tuple tp;
   int32_t cnt = 0;
   auto* txn = exec_ctx_->GetTransaction();
-  auto temp_ts = txn->GetTransactionTempTs();
+  // Get promary key.
+  IndexInfo* primary_key = nullptr;
+  for (size_t i = 1;i < indexs_info_.size();++i) {
+    if (indexs_info_[i]->is_primary_key_) {
+      std::swap(indexs_info_[i], indexs_info_[0]);
+      primary_key = indexs_info_[0];
+      break;
+    }
+  }
+
+  // Check if all tuples already exist in the index. 
+  // If it exists, abort the transaction.
+  std::vector<Tuple> tuples;
   while (child_executor_->Next(&tp, rid)) {
+    auto key = tp.KeyFromTuple(table_info_->schema_, *primary_key->index_->GetKeySchema(), primary_key->index_->GetKeyAttrs());
+    std::vector<RID> res;
+    primary_key->index_->ScanKey(key, &res, txn);
+    if (res.size() > 0) {
+      txn->SetTainted();
+      throw ExecutionException("duplicate primary key");
+    }
+    tuples.emplace_back(std::move(tp));
+  }
+
+
+  for (auto & tuple : tuples) {
     // insert data
-    auto rid_opt = table_info_->table_->InsertTuple({temp_ts, false}, tp);
+    auto rid_opt = table_info_->table_->InsertTuple({txn->GetTransactionTempTs(), false}, tuple);
     if (!rid_opt.has_value()) {
       continue;
     }
-    txn->AppendWriteSet(table_info_->oid_, rid_opt.value());
 
     // update indexs
     for (auto &index_info : indexs_info_) {
-      auto key =
-          tp.KeyFromTuple(table_info_->schema_, *index_info->index_->GetKeySchema(), index_info->index_->GetKeyAttrs());
+      auto key = 
+          tuple.KeyFromTuple(table_info_->schema_, *index_info->index_->GetKeySchema(), index_info->index_->GetKeyAttrs());
       index_info->index_->InsertEntry(key, rid_opt.value(), exec_ctx_->GetTransaction());
     }
+    txn->AppendWriteSet(table_info_->oid_, rid_opt.value());
     cnt++;
   }
   Schema sch{{Column{"num_of_inserted", TypeId::INTEGER}}};
